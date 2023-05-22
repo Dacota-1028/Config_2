2208b85ffed1dac4187ca42944362117cbac3382
Dacota-1028 <101526385+Dacota-1028@users.noreply.github.com>
2023-05-22 15:48:50 -0500

    Add files via upload

---
+++ demo/src/MyClass.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/MyClass.h"
+using namespace std;
+
+void MyClass::saludo(){
+    cout << "Hola Mundo desde una clase" << endl;
+}
---
+++ demo/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/MyClass.h"
+#include <cstdio>
+using namespace std;
+
+int main(){
+    MyClass obj;
+    obj.saludo();
+    getchar();
+    return 0;
+}
